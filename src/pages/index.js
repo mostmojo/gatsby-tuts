3cca9a56f78d02b371b62ab05e61e1097775cf35
Jacob <jacob.papageorgiou@gmail.com>
2021-06-02 00:19:36 +0300

    Add image optimizer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import * as React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
+import Img from "gatsby-image"
 
-export default function Home() {
+export default function Home({data}) {
+  console.log(data)
   return (
     <Layout>
       <section className={styles.header}>
@@ -15,12 +17,20 @@ export default function Home() {
             My portfolio projects
           </Link>
         </div>
-        <img
-          src="/banner.png"
-          alt="sites banner"
-          style={{ maxWidth: "100%" }}
-        />
+        <Img fluid ={data.file.childImageSharp.fluid}/>
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+query Banner {
+  file(relativePath: {eq: "banner.png"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+}
+`
